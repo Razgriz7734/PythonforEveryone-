68613d71ff5494e9ed0e8926d75393690b6ccafc
Razgriz7734 <54412515+Razgriz7734@users.noreply.github.com>
2019-09-17 12:12:15 -0400

    Create Quiz.py

---
+++ Quiz.py
@@ -0,0 +1,8 @@
+#Sebastian Olivieri
+#A basic quiz and directory for what is learned in Python oer the course of COP 1500
+
+print("Hello world! Isn't today a wonderful day?")
+print(25-6)
+print(38*3-14)
+print(63/7)
+print(8**2)
